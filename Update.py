f8c30305a93a7c356b9d46db2e0feb99e82c089c
Ray-PITT <62837921+Ray-PITT@users.noreply.github.com>
2024-05-06 14:39:29 +0200

    Add files via upload

---
+++ Update.py
@@ -0,0 +1,23 @@
+import os
+
+
+#MAJ de l'OS
+def update_system():
+    os.system("sudo apt-get update")
+    os.system("sudo apt-get upgrade")
+#MAJ De nmap ainsi que sa bdd de script
+def nmap_update():
+    os.system("sudo apt install --only-upgrade nmap")
+    os.system("sudo nmap --script-updatedb")
+#MAJ HASHCAT
+def hashcat_update():
+    os.system("sudo apt install --only-upgrade hashcat")
+#MAJ HYDRA
+def hydra_update():
+    os.system("sudo apt install --only-upgrade hydra")
+
+
+
+
+
+
