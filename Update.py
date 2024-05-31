86d53f75fa04ea7c3c4ce211c0dbf14782a1b9a6
wan-o-wan <erwan51400.21@hotmail.fr>
2024-05-31 12:14:51 +0200

    update update.py

--- Update.py
+++ Update.py
@@ -6,14 +6,14 @@ def update_system():
     os.system("sudo apt-get update")
     os.system("sudo apt-get upgrade")
 #MAJ De nmap ainsi que sa bdd de script
-def nmap_update():
+# def nmap_update():
     os.system("sudo apt install --only-upgrade nmap")
     os.system("sudo nmap --script-updatedb")
 #MAJ HASHCAT
-def hashcat_update():
+# def hashcat_update():
     os.system("sudo apt install --only-upgrade hashcat")
 #MAJ HYDRA
-def hydra_update():
+# def hydra_update():
     os.system("sudo apt install --only-upgrade hydra")
 
 update_system()
